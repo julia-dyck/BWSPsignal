a6c6b785eed2dc4468c438e569267c3f0e65b313
julia-dyck <julia.alexandra.dyck@gmail.com>
2024-12-06 10:29:59 +0100

    add test data

---
+++ R/testdat.R
@@ -0,0 +1,6 @@
+#' test data for development of simstudy code
+#'
+#' @description generated with \code{datagen} command with
+#' N = 100, br = 0.1, adr.rate = 1, adr.when = 0.5, adr.relsd = 0.05, censor = 365.
+#'
+"testdat"
