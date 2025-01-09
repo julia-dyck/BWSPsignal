6179823fb40ec8643d960c61f08496eeb8c4279f
julia-dyck <julia.alexandra.dyck@gmail.com>
2025-01-09 14:06:19 +0100

    add documentation, rename dat argument

--- R/fit.prep_function.R
+++ R/fit.prep_function.R
@@ -1,13 +1,14 @@
 #' Prepare Bayesian pgW fitting
 #'
-#' Contains data preparation for stanmodel.
+#' Formatting of simulated data generated with \code{link{datagen_tte}} as building
+#' block for tuning the Bayesian WSP test via simulation study.
 #'
 #'
-#' @param survdat time-event-sample
-#' @param pc one parameter combination used in simulation study
+#' @param ttedat time-to-event data set
+#' @param pc vector representing one parameter combination used in simulation study
 #'
-#' @return a list containing all information to be inserted into fit.fgg, fit.fll,
-#'         fit.ggg, or fit.lll function.
+#' @return list containing all information to be inserted as \code{datstan} 
+#' argument into the \code{\link{fit_pgw_tte} function.
 #'
 #'
 #' @export
@@ -15,15 +16,15 @@
 
 
 
-fit.prep = function(survdat, pc){
-  # survdat = data set in time-event-format generated from pc
+fit.prep = function(ttedat, pc){
+  # ttedat = data set in time-event-format generated from pc
   # pc contains N, br, adr.rate, adr.when, adr.relsd, censor, dist.ass, adr.ass
   adr.assumption = pc[8]
 
   # 1. prior starting values reflecting hyp: "no adr risk over time"
   # data reformatting
   if(adr.assumption == "none"){
-    datstan = tte2standat(dat = survdat,
+    datstan = tte2standat(dat = ttedat,
                                 scale.mean = 1, scale.sd = 10,
                                 shape.mean = 1, shape.sd = 10,
                                 powershape.mean = 1, powershape.sd = 10)
@@ -32,7 +33,7 @@ fit.prep = function(survdat, pc){
   # 2. prior starting values reflecting hyp: "adr occuring at beginning of observation period"
   # data reformatting
   if(adr.assumption == "beginning"){
-    datstan = tte2standat(dat = survdat,
+    datstan = tte2standat(dat = ttedat,
                                       scale.mean = 1, scale.sd = 10,
                                       shape.mean = 0.207, shape.sd = 10,
                                       powershape.mean = 1, powershape.sd = 10)
@@ -41,7 +42,7 @@ fit.prep = function(survdat, pc){
   # 3. prior starting values reflecting hyp: "adr occuring towards end of observation period"
   # data reformatting
   if(adr.assumption == "end"){
-    datstan = tte2standat(dat = survdat,
+    datstan = tte2standat(dat = ttedat,
                                       scale.mean = 300, scale.sd = 10,
                                       shape.mean = 4, shape.sd = 10,
                                       powershape.mean = 1, powershape.sd = 10)
@@ -50,7 +51,7 @@ fit.prep = function(survdat, pc){
   # 4. prior starting values reflecting hyp: "adr occuring within middle of the observation period"
   # data reformatting
   if(adr.assumption == "middle"){
-    datstan = tte2standat(dat = survdat,
+    datstan = tte2standat(dat = ttedat,
                                       scale.mean = 20, scale.sd = 10,
                                       shape.mean = 5.5, shape.sd = 10,
                                       powershape.mean = 14, powershape.sd = 10)
