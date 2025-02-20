13c7c9da44368098939a88de8e5380e19abc82ca
Dyck <j.dyck@uni-bielefeld.de>
2025-02-20 15:39:18 +0100

    sim.setup_dgp_pars

--- R/sim.setup_simpars.R
+++ R/sim.setup_simpars.R
@@ -10,42 +10,45 @@
 
 sim.setup_dgp_pars = function(N,           # dgp parameters
                                br,
-                               adr.rate,
+                               adr.rate, # without control 0
                                adr.relsd,
                                study.period
                                ){
   
   ## TODO prior belief sachen erstmal wieder raus, fälle seperat aufsetzen und 
-  ## in sim.setup_sim_scenarios oder so mergen
+  ## 
   pc_with_adr = expand.grid(
-    adr.ass = c("beginning", "middle", "end", "none"),
-    dist.ass = c("fix.gam.gam", "gam.gam.gam", "fix.log.log", "log.log.log"),
-    study.period = 365,
-    adr.relsd = c(0.05),
-    adr.when = c(0.25, 0.5, 0.75),
-    adr.rate = c(0.5, 1),         
-    br = 0.1,
-    N = c(500, 3000, 5000)
+    study.period = study.period,
+    adr.relsd = adr.relsd,
+    adr.rate = adr.rate,         
+    br = br,
+    N = N
   )
   
   pc_no_adr = expand.grid(
-    adr.ass = c("beginning", "middle", "end", "none"),
-    dist.ass = c("fix.gam.gam", "gam.gam.gam", "fix.log.log", "log.log.log"),
-    study.period = 365,
-    adr.relsd = c(0.05),
-    adr.when = c(0),
-    adr.rate = c(0),
-    br = 0.1,
-    N = c(500, 3000, 5000)
+    study.period = study.period,
+    adr.relsd = rel.sd,
+    adr.rate = 0,
+    br = br,
+    N = N
   )
+  
+  dgp_pc = rbind(pc_no_adr,
+             pc_with_adr)
+  return(dgp_pc)
 }
-
 
 
 sim.setup_mod_pars = function(mod,
                               prior.dist,
-                              df.of.believes){
+                              df.of.believes  # ehemals weniger allgemein "adr.when"
+                              ){
   ## sets up an expand.grid matrix with cols:
   # mod, prior.dist, belief.abbreviation
   # each belief.abbreviation is linked to one row of df.of.believes, that then again contains all prior means and sds
+}
+
+
+sim.setup_test_pars = function(){
+  
 }
