5e0d95f1863fc1f67ceff3a149a6dba51b026277
Dyck <j.dyck@uni-bielefeld.de>
2025-03-04 17:37:19 +0100

    adjust looping for new sim_pars setup

--- simulationstudy_template/run_simulation.R
+++ simulationstudy_template/run_simulation.R
@@ -13,9 +13,11 @@ resultpath = paste0(getwd(), "/results_raw")
 
 # fct with huge pc list as input sim.run(pc.list = pc)
 
-for(pcind in 1:nrow(pc$dgp)){
-  for(i in 1:10){
-    sim.repeat.1.scenario(batch.nr=i, reps = 2, pc = pc[pcind,],
+
+
+for(dgpind in 1:nrow(pc_list$dgp)){
+  for(batchind in 1:nr.batches){
+    sim.repeat.1.scenario(batch.ind = batchind, reps.per.batch = 10, dgp = pc_list$dgp[dgpind,],
                           save = T, path = resultpath)
   }
 }
@@ -23,3 +25,40 @@ for(pcind in 1:nrow(pc$dgp)){
 
 
 ## END OF DOC
+
+## new, more flexible version only depending on huge pc_list object
+
+
+
+
+for(ind.dgp in 1:nrow(pc_list$dgp)){      # go through dgp scenarios (per row)
+  if(nrow(pc_list$fit$w)>0){
+    for(ind.fitw in 1:nrow(pc_list$fit$w)){ # go through weibull fitting parameter combis
+      
+      pc = sim.gather_pc_vect(pc_list$dgp[ind.dgp,], pc_list$fit$w[ind.fitw,])
+      print(pc)
+      # for(ind.batch in 1:nr.batches){
+      #   sim.repeat.1.scenario(batch.ind = ind.batch, 
+      #                         reps.per.batch = 10, 
+      #                         pc = pc,
+      #                         save = T, 
+      #                         path = resultpath)
+      # }
+    }
+  }
+  
+  if(nrow(pc_list$fit$w)>0){
+    for(ind.fitdw in 1:nrow(pc_list$fit$dw)){
+      print(paste(row, "dgp", ind.dgp, "fitdw", ind.fitdw))
+      row = row +1
+    }
+  }
+  
+  if(nrow(pc_list$fit$pgw)>0){
+    for(ind.fitpgw in 1:nrow(pc_list$fit$pgw)){
+      print(paste(row, "dgp", ind.dgp, "fitpgw", ind.fitpgw))
+      row = row +1
+    }
+  }
+  
+}
