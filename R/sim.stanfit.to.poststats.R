83b6688e7e4635d109716cb3e626ac0bca3c1336
julia-dyck <julia.alexandra.dyck@gmail.com>
2025-03-20 16:07:19 +0100

    rearrange output per model fit / generalize tte.dist options

--- R/sim.stanfit.to.poststats.R
+++ R/sim.stanfit.to.poststats.R
@@ -35,58 +35,34 @@ sim.stanfit.to.poststats = function(pc, stanfit.object, cred.niveaus = seq(0.5, 
   
   if(pc$tte.dist == "w"){
     obj = stanfit.object
+    obj = stanfit.object
     
     # extract running time
     run.min = sum(rstan::get_elapsed_time(obj))/60 # in minutes
+    names(run.min) = "run.min"
     
-    # add in: "if the scale parameter is fittet: ...
-    if(pc$prior.dist == "fl" || pc$prior.dist == "fg"){
-      post_summary = rstan::summary(obj, pars = c("theta", "nu"), probs = c())$summary
-      poststats = list(th = post_summary["theta",], nu = post_summary["nu",], ga = post_summary["gamma",])
-    }
-    # else (ie. if the scale parameter is fix):
-    if(pc$prior.dist == "ll" || pc$prior.dist == "gg"){
-      post_summary = rstan::summary(obj, pars = c("nu", "gamma"), probs = c())$summary
-      poststats = list(th = rep(NA, 5), nu = post_summary["nu",], ga = post_summary["gamma",])
-    }
-    
-    th.post.stats = poststats$th
-    names(th.post.stats) = paste0("th.po.", names(poststats$nu))
+    # summary statistics for shape parameters
+    post_summary = rstan::summary(obj, pars = c("nu"), probs = c())$summary
+    poststats = list(nu = post_summary["nu",])
     
     nu.post.stats = poststats$nu
     names(nu.post.stats) = paste0("nu.po.", names(nu.post.stats))
     
-    ga.post.stats = poststats$ga
-    names(ga.post.stats) = paste0("ga.po.", names(ga.post.stats))
     
-    
-    ######## extraction of posterior samples for credible interval calculation
-    post.sample = rstan::extract(obj, pars = c("nu", "gamma"))
+    # extraction of posterior samples for credible interval calculation
+    post.sample = rstan::extract(obj, pars = c("nu"))
     # results in
     # ## post.sample$nu = posterior sample of nu, representing the posterior distribution
-    # ## post.sample$gam = posterior sample of gamma, representing the posterior distribution
     
-    # credibility/confidence niveaus 1-alpha
-    # 50% ETI and HDI
-    # 55% ETI and HDI
-    # ...
-    # 95% ETI and HDI
     
     # calculate equal tailed intervals (ETIs)
+    
     nu.eti = matrix(nrow = length(cred.niveaus), ncol = 2)
     for(i in 1:length(cred.niveaus)){
       nu.eti[i,] = stats::quantile(post.sample$nu, probs = 0.5 + c(-1,1)*cred.niveaus[i]/2)
     }
     nu.eti = c(t(nu.eti)) # transform to vector
     names(nu.eti) = paste0("nu.eti",rep(cred.niveaus, each = 2), rep(c("l","u"), times = length(cred.niveaus)) )
-    
-    
-    ga.eti = matrix(nrow = length(cred.niveaus), ncol = 2)
-    for(i in 1:length(cred.niveaus)){
-      ga.eti[i,] = stats::quantile(post.sample$gamma, probs = 0.5 + c(-1,1)*cred.niveaus[i]/2)
-    }
-    ga.eti = c(t(ga.eti)) # transform to vector
-    names(ga.eti) = paste0("ga.eti",rep(cred.niveaus, each = 2), rep(c("l","u"), times = length(cred.niveaus)) )
     
     
     # calculate highest density intervals (HDIs)
@@ -96,30 +72,107 @@ sim.stanfit.to.poststats = function(pc, stanfit.object, cred.niveaus = seq(0.5, 
     names(nu.hdi) = paste0("nu.hdi",rep(cred.niveaus, each = 2), rep(c("l","u"), times = length(cred.niveaus)) )
     
     
-    ga.hdi = t(sapply(cred.niveaus, HDInterval::hdi, object = post.sample$gamma))
-    ga.hdi = c(t(ga.hdi)) # transform to vector
-    names(ga.hdi) = paste0("ga.hdi",rep(cred.niveaus, each = 2), rep(c("l","u"), times = length(cred.niveaus)) )
-    
     # calculate percentiles
     
     nu.per = stats::quantile(post.sample$nu, probs = (0:100)/100)
     names(nu.per) = paste0("nu.per", names(nu.per))
     
-    ga.per = stats::quantile(post.sample$gamma, probs = (0:100)/100)
-    names(ga.per) = paste0("ga.per", names(ga.per))
     
-    ret.vect = t(as.data.frame(c(pc,
-                                 run.min,
-                                 th.post.stats,
-                                 nu.post.stats, nu.eti, nu.hdi, nu.per,
-                                 ga.post.stats, ga.eti, ga.hdi, ga.per)))
+    # vector to be returned
+    
+    ret.vect = data.frame(t(c(run.min,
+                              nu.post.stats, nu.eti, nu.hdi, nu.per)))
+    
+    ret.vect = cbind(pc, ret.vect)
+    
   }
   
   if(pc$tte.dist == "dw"){
     obj = stanfit.object$uncens
     obj_c = stanfit.object$cens
     
+    # extract running time
+    
     run.min = sum(rstan::get_elapsed_time(obj) + rstan::get_elapsed_time(obj_c))/60 # in minutes
+    names(run.min) = "run.min"
+    
+    # uncensored: 
+    # summary statistics for shape parameter
+    post_summary = rstan::summary(obj, pars = c("nu"), probs = c())$summary
+    poststats = list(nu = post_summary["nu",])
+    
+    nu.post.stats = poststats$nu
+    names(nu.post.stats) = paste0("nu.po.", names(nu.post.stats))
+    
+    
+    # extraction of posterior samples for credible interval calculation
+    post.sample = rstan::extract(obj, pars = c("nu"))
+    
+    # calculate equal tailed intervals (ETIs)
+    
+    nu.eti = matrix(nrow = length(cred.niveaus), ncol = 2)
+    for(i in 1:length(cred.niveaus)){
+      nu.eti[i,] = stats::quantile(post.sample$nu, probs = 0.5 + c(-1,1)*cred.niveaus[i]/2)
+    }
+    nu.eti = c(t(nu.eti)) # transform to vector
+    names(nu.eti) = paste0("nu.eti",rep(cred.niveaus, each = 2), rep(c("l","u"), times = length(cred.niveaus)) )
+    
+    # calculate highest density intervals (HDIs)
+    
+    nu.hdi = t(sapply(cred.niveaus, HDInterval::hdi, object = post.sample$nu))
+    nu.hdi = c(t(nu.hdi)) # transform to vector
+    names(nu.hdi) = paste0("nu.hdi",rep(cred.niveaus, each = 2), rep(c("l","u"), times = length(cred.niveaus)) )
+    
+    # calculate percentiles
+    
+    nu.per = stats::quantile(post.sample$nu, probs = (0:100)/100)
+    names(nu.per) = paste0("nu.per", names(nu.per))
+    
+    
+    # censored: 
+    # summary statistics for shape parameter
+    post_summary = rstan::summary(obj_c, pars = c("nu"), probs = c())$summary
+    poststats = list(ga = post_summary["nu",])
+    
+    ga.post.stats = poststats$ga
+    names(ga.post.stats) = paste0("ga.po.", names(ga.post.stats))
+    
+    
+    # extraction of posterior samples for credible interval calculation
+    post.sample = rstan::extract(obj_c, pars = c("nu"))
+    
+    
+    # calculate equal tailed intervals (ETIs)
+    
+    ga.eti = matrix(nrow = length(cred.niveaus), ncol = 2)
+    for(i in 1:length(cred.niveaus)){
+      ga.eti[i,] = stats::quantile(post.sample$nu, probs = 0.5 + c(-1,1)*cred.niveaus[i]/2)
+    }
+    ga.eti = c(t(ga.eti)) # transform to vector
+    names(ga.eti) = paste0("ga.eti",rep(cred.niveaus, each = 2), rep(c("l","u"), times = length(cred.niveaus)) )
+    
+    
+    # calculate highest density intervals (HDIs)
+    
+    ga.hdi = t(sapply(cred.niveaus, HDInterval::hdi, object = post.sample$nu))
+    ga.hdi = c(t(ga.hdi)) # transform to vector
+    names(ga.hdi) = paste0("ga.hdi",rep(cred.niveaus, each = 2), rep(c("l","u"), times = length(cred.niveaus)) )
+    
+    
+    # calculate percentiles
+    
+    ga.per = stats::quantile(post.sample$nu, probs = (0:100)/100)
+    names(ga.per) = paste0("ga.per", names(ga.per))
+    
+    
+    
+    # vector to be returned
+    
+    ret.vect = data.frame(t(c(run.min,
+                              nu.post.stats, nu.eti, nu.hdi, nu.per,
+                              ga.post.stats, ga.eti, ga.hdi, ga.per)))
+    
+    ret.vect = cbind(pc, ret.vect)
     
   }
   
@@ -127,21 +180,14 @@ sim.stanfit.to.poststats = function(pc, stanfit.object, cred.niveaus = seq(0.5, 
     obj = stanfit.object
     
     # extract running time
+    
     run.min = sum(rstan::get_elapsed_time(obj))/60 # in minutes
+    names(run.min) = "run.min"
     
-    # add in: "if the scale parameter is fittet: ...
-    if(pc$prior.dist == "fl" || pc$prior.dist == "fg"){
-      post_summary = rstan::summary(obj, pars = c("theta", "nu", "gamma"), probs = c())$summary
-      poststats = list(th = post_summary["theta",], nu = post_summary["nu",], ga = post_summary["gamma",])
-    }
-    # else (ie. if the scale parameter is fix):
-    if(pc$prior.dist == "ll" || pc$prior.dist == "gg"){
-      post_summary = rstan::summary(obj, pars = c("nu", "gamma"), probs = c())$summary
-      poststats = list(th = rep(NA, 5), nu = post_summary["nu",], ga = post_summary["gamma",])
-    }
+    # summary statistics for shape parameters
     
-    th.post.stats = poststats$th
-    names(th.post.stats) = paste0("th.po.", names(poststats$nu))
+    post_summary = rstan::summary(obj, pars = c("nu", "gamma"), probs = c())$summary
+    poststats = list(nu = post_summary["nu",], ga = post_summary["gamma",])
     
     nu.post.stats = poststats$nu
     names(nu.post.stats) = paste0("nu.po.", names(nu.post.stats))
@@ -152,15 +198,6 @@ sim.stanfit.to.poststats = function(pc, stanfit.object, cred.niveaus = seq(0.5, 
     
     ######## extraction of posterior samples for credible interval calculation
     post.sample = rstan::extract(obj, pars = c("nu", "gamma"))
-    # results in
-    # ## post.sample$nu = posterior sample of nu, representing the posterior distribution
-    # ## post.sample$gam = posterior sample of gamma, representing the posterior distribution
-    
-    # credibility/confidence niveaus 1-alpha
-    # 50% ETI and HDI
-    # 55% ETI and HDI
-    # ...
-    # 95% ETI and HDI
     
     # calculate equal tailed intervals (ETIs)
     nu.eti = matrix(nrow = length(cred.niveaus), ncol = 2)
@@ -198,26 +235,26 @@ sim.stanfit.to.poststats = function(pc, stanfit.object, cred.niveaus = seq(0.5, 
     ga.per = stats::quantile(post.sample$gamma, probs = (0:100)/100)
     names(ga.per) = paste0("ga.per", names(ga.per))
     
-    ret.vect = t(as.data.frame(c(pc,
-                                 run.min,
-                                 th.post.stats,
-                                 nu.post.stats, nu.eti, nu.hdi, nu.per,
-                                 ga.post.stats, ga.eti, ga.hdi, ga.per)))
+    
+    # vector to be returned
+    
+    ret.vect = data.frame(t(c(run.min,
+                              nu.post.stats, nu.eti, nu.hdi, nu.per,
+                              ga.post.stats, ga.eti, ga.hdi, ga.per)))
+    
+    ret.vect = cbind(pc, ret.vect)
   }
 
- rownames(ret.vect) = NULL
- return(ret.vect)
+  rownames(ret.vect) = NULL
+  return(ret.vect)
 }
 
 
 
-
-# function testin --------------------------------------------------------------
-
-# stanfit.to.poststats(stanfit.object = testout[[1]], cred.niveaus = seq(0.5, 0.95, by = 0.05))
-# stats.out = stanfit.to.poststats(testout[[2]])
-# stats.out
-# dim(stats.out)
-# View(stats.out)
+# ## test
+# 
+# # mod = inner mod object in sim.fit.to.1.sample fct
+# post = sim.stanfit.to.poststats(pc = pc, stanfit.object = mod)
+# View(post)
 
 ## END of Doc
