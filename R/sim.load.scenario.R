f293e049aa12f4ebfc6562be8da13dcb3b8cedd7
julia-dyck <julia.alexandra.dyck@gmail.com>
2025-03-21 14:49:21 +0100

    adjust to new pc format

--- R/sim.load.scenario.R
+++ R/sim.load.scenario.R
@@ -18,30 +18,19 @@
 #'
 
 sim.load.scenario = function(wd, pc, batchnr = 1){
-  N = pc[1]
-  br = pc[2]
-  adr.rate = pc[3]
-  adr.when = pc[4]
-  adr.relsd = pc[5]
-  study.period = pc[6]
-  dist.ass = pc[7]
-  adr.ass = pc[8]
-  # load the first batch
-  filename = paste(
-                   N, br, adr.rate, adr.when, adr.relsd,
-                   study.period, dist.ass, adr.ass,
-                   "bADR_sim", batchnr,".RData", sep = "_")
+
+  filename = paste(c(pc, "bADR_sim", batchnr, ".RData") ,collapse="_")
+  
   path = paste0(wd, "/", filename)
-  #return(path)
+  
+  # return(path)
   load(file = path)
   res = res.batch
   return(res)
 }
 
-#load_scenario(wd = "C:/Users/jdyck/sciebo/bADR_simstudyres01",
-#              pc = c(500, 0.1, 0.5, 0.5, 0.05, 365, 1,1), batchnr = 1)
 
 
 
 
-
+## END OF DOC
