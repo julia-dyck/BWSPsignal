a0ff7b5954fe23af76d29186c21589f6a70c6618
julia-dyck <julia.alexandra.dyck@gmail.com>
2025-04-10 15:22:48 +0200

    outsource sim.gather_pc_vect fct from sim.setup_simpars into its own file

---
+++ R/sim.gather_pc_vect.R
@@ -0,0 +1,23 @@
+#' Extract parameter vector from pc_list
+#'
+#' Inner function to extract one simulation scenario's parameter from one 
+#' data generating process specification and one fitting parameter specification.
+#'
+#'
+#' @param dgp_pars_vect  vector containing 6 elements, namely:
+#' * the sample size N,
+#' * the background rate br,
+#' * the ADR rate adr,
+#' * the ADR mean time quantile percentage,
+#' * the ADR relative standard deviation from the mean time,
+#' * the study end time point.
+#' @param fit_pars_vect vector containing 3 elements, namely:
+#' * the time-to-event distribution (either \code{"w", "dw"} or \code{"pgw"}),
+#' * the prior distribution (either \code{"fl", "ll", "fg"} or \code{"gg"}),
+#' * prior belief (one of the characters specified in \link\code{sim.setup_simpars}}).
+#' 
+
+sim.gather_pc_vect = function(dgp_pars_vect, fit_pars_vect){
+  pc_vect = data.frame(dgp_pars_vect, fit_pars_vect)
+  return(pc_vect)
+}
