00a76fd87e4e3db7a697f5a901e288d8dbd046b9
julia-dyck <julia.alexandra.dyck@gmail.com>
2025-04-11 14:46:45 +0200

    embed in fct (in work)

--- simulationstudy_template/eval_execution_times.R
+++ simulationstudy_template/eval_execution_times.R
@@ -2,7 +2,7 @@
 
 
 # calculate execution times for different dist. assumptions
-dist.ass.factor = res$dist.ass %>% as.numeric() %>% as.factor()
+prior.dist.factor = res$prior.dist %>% as.factor()
 
 run.min.numeric = res$run.min %>% as.numeric() # in minutes
 run.h.numeric = run.min.numeric/60             # in hours
@@ -11,7 +11,7 @@ run.h.numeric = run.min.numeric/60             # in hours
 #### boxplot of execution times ------------------------------------------------
 
 time.df = data.frame(dist.ass.factor, run.min.numeric)
-xlabs = c("fix-log-log", "log-log-log", "fix-gam-gam", "gam-gam-gam")
+xlabs = unique(dist.ass.factor)
 
 p = ggplot(time.df, aes(dist.ass.factor, run.min.numeric)) +
   geom_boxplot(fill="lightgray") +
@@ -37,6 +37,63 @@ colnames(run.time.summary) <- xlabs
 
 
 run.time.summary
+
+
+### new -------------
+
+eval.execution_times = function(pc_list){
+  
+  # obtain res table
+  tryCatch({
+    load(paste0(pc_list$add$resultpath, "/res.RData"))
+    message("res.RData successfully loaded")
+  }, error = function(cond) {
+    sim.merge_results(pc_list, save = T)
+    load(paste0(pc_list$add$resultpath, "/res.RData"))
+    print(" batches merged and loaded")
+  })
+  
+  # select relevant variables
+  time.df = res[, c("tte.dist", "prior.dist", "run.min")]
+  # adjust format
+  time.df$tte.dist <- as.factor(unlist(time.df$tte.dist))
+  time.df$prior.dist <- as.factor(unlist(time.df$prior.dist))
+  time.df$run.min = as.numeric(unlist(time.df$run.min)) 
+  
+  # execution time dist per tte.dist and prior.dist
+  time.summaries = dplyr::summarise(
+    dplyr::group_by(time.df, tte.dist, prior.dist),
+    min = min(run.min, na.rm = TRUE),
+    first_qu = quantile(run.min, 0.25, na.rm = TRUE),
+    median = median(run.min, na.rm = TRUE),
+    mean = mean(run.min, na.rm = TRUE),
+    third_qu = quantile(run.min, 0.75, na.rm = TRUE),
+    max = max(run.min, na.rm = TRUE),
+    .groups = "drop"
+  )
+  
+  # plot
+  p = ggplot2::ggplot(time.df, ggplot2::aes(x = prior.dist, y = run.min)) +
+    ggplot2::geom_boxplot(fill="lightgray") +
+    ggplot2::facet_wrap(~ tte.dist) +
+    ggplot2::labs(
+      x = "Prior Distribution",
+      y = "Run Time (min)",
+      title = "Execution time in minutes") +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(legend.position = "none")
+  
+  
+  return(list(summary = time.summaries, # for overview
+              res.ex_times.table = time.df,       # for option to plot manually in preferred format
+              plot = p
+              ))
+  
+}
+
+
+eval.execution_times(pc_list)
+
 
 
 ## END OF DOC
