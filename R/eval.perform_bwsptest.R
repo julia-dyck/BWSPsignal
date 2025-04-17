3d9df898f3ec2b5555c735c151b6a1d41ff47e70
julia-dyck <julia.alexandra.dyck@gmail.com>
2025-04-17 19:26:15 +0200

    embed eval_auc_calc.R in a fct (start)

---
+++ R/eval.perform_bwsptest.R
@@ -0,0 +1,31 @@
+
+eval.calc_auc = function(pc_list){
+  # load res
+  if (!exists("res")) { 
+    # obtain res table
+    tryCatch({
+      load(paste0(pc_list$add$resultpath, "/res.RData"))
+      message("res.RData successfully loaded")
+    }, error = function(cond) {
+      sim.merge_results(pc_list, save = T)
+      load(paste0(pc_list$add$resultpath, "/res.RData"))
+      message(" batches merged and loaded")
+    })
+  }
+  else{
+    message("Object `res` loaded in current environment is used to extract effective sample sizes.")
+  }
+  
+  
+  # add rope lower and upper bound for to each row depending on 
+  # ## tte.dist, 
+  # ## prior.dist (+ prior mean + sd under prior.believe = "none"), 
+  rope.infos = dplyr::filter(do.call(dplyr::bind_rows, pc_list$fit), prior.belief == "none")
+  rope.infos = mutate(rope.l = )
+  
+  
+  # go through pc_list$test rows
+  # extract ci_boundaries depending on post.ci.type & cred.level (using regex?)
+  
+  
+}
