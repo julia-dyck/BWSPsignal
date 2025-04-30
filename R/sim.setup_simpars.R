a7491245e098dfc16b4ceb14a353412be7af3258
julia-dyck <julia.alexandra.dyck@gmail.com>
2025-04-30 10:06:33 +0200

    add in adr.when.name (to evaluate auc dep. on distance between simulated truth and prior belief later)

--- R/sim.setup_simpars.R
+++ R/sim.setup_simpars.R
@@ -7,7 +7,7 @@
 #' @param br A scalar or vector of background rates.
 #' @param adr.rate A scalar or vector of adverse drug reaction rates.
 #' @param adr.when A scalar or vector of expected event times (relative number, e.g. 0.5 matches half of study period).
-#' @param adr.when.names A vector of description/short name of expected event times (ie. name of simulated truth)
+#' @param adr.when.name A vector of description/short name of expected event times (ie. name of simulated truth)
 #' @param adr.relsd A scalar or vector of relative standard deviations from the adverse drug reaction times.
 #' @param study.period A scalar specifying the length of the study period.
 #' 
@@ -25,18 +25,19 @@ sim.setup_dgp_pars = function(N,           # dgp parameters
                               br,
                               adr.rate,
                               adr.when,
-                              adr.when.names,
+                              adr.when.name,
                               adr.relsd,
                               study.period 
                                ){
   if(any(adr.rate == 0)){ # separate control and adr>0 cases
     adr.rate = adr.rate[-(which(adr.rate==0))]
   }
+  
   else{ # its fine, but notify user that control case is necessary
     warning("Control case (adr.rate = 0) necessary for simulation study. Value 0 is added to specified vector adr.rate.")
   }
   
-  if(length(adr.when) != length(adr.when.names)){
+  if(length(adr.when) != length(adr.when.name)){
     stop("Length of adr.when and adr.when.names must be equal.")
   }
   
@@ -49,12 +50,16 @@ sim.setup_dgp_pars = function(N,           # dgp parameters
     br = br,
     N = N
   )
+  adr.when_plus_name = data.frame(adr.when = adr.when, adr.when.name = adr.when.name)
+  pc_with_adr = dplyr::left_join(pc_with_adr, adr.when_plus_name, by = "adr.when")
+  
   # par combis with adr.rate = 0 (control), adr.when then also set to 0
 
   pc_no_adr = expand.grid(       
     study.period = study.period,
     adr.relsd = NA,
     adr.when = NA,
+    adr.when.name = "none",
     adr.rate = 0,
     br = br,
     N = N
@@ -63,20 +68,22 @@ sim.setup_dgp_pars = function(N,           # dgp parameters
   dgp_pc = rbind(pc_no_adr,
              pc_with_adr)
   
+  
   dgp_pc = dgp_pc[, ncol(dgp_pc):1]
   dgp_pc = dgp_pc[order(dgp_pc$N),]
   rownames(dgp_pc) <- 1:nrow(dgp_pc)
   return(dgp_pc)
 }
 
-# dgp_pc = sim.setup_dgp_pars(N = c(500, 3000, 5000),
-#                             br = 0.1,
-#                             adr.rate = c(0.5, 1),
-#                             adr.when = c(0.25, 0.5, 0.75),
-#                             adr.relsd = c(0.05),
-#                             study.period = 365)
-# 
-# dgp_pc
+dgp_pc = sim.setup_dgp_pars(N = c(500, 3000, 5000),
+                            br = 0.1,
+                            adr.rate = c(0.5, 1),
+                            adr.when = c(0.25, 0.5, 0.75),
+                            adr.when.name = c("beginning", "middle", "end"),
+                            adr.relsd = c(0.05),
+                            study.period = 365)
+
+dgp_pc
 
 #### model fitting parameter specification
 
