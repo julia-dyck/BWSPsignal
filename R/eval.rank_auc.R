4ec03a108a0bc61c7b4a6b722ce1d21d6048088a
julia-dyck <julia.alexandra.dyck@gmail.com>
2025-05-08 19:12:55 +0200

    setup ranking fct

---
+++ R/eval.rank_auc.R
@@ -0,0 +1,18 @@
+#
+
+
+
+
+eval.rank_auc = function(aucs,
+                         tte.dist.filter = c("w", "dw", "pgw"), # option to exclude/focus on specific tte distributions (after having a look at execution times/convergence/n_eff)
+                         prior.dist.filter = c("fl", "fg", "ll", "gg")
+                         # potentially add the other scenario pars as well
+){
+  require(dplyr)
+
+  
+  
+  
+  
+  
+}
