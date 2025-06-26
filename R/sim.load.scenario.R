c214a2a91eedc6b548b89982e6298de93255d7ce
julia-dyck <julia.alexandra.dyck@gmail.com>
2025-06-26 12:24:21 +0200

    if choice to load bayesian or freq results

--- R/sim.load.scenario.R
+++ R/sim.load.scenario.R
@@ -1,8 +1,7 @@
 #' Load simulated scenario
 #' 
 #' Load all simulations of a simulated scenario (obtained using 
-#' \code{\link{sim.repeat.1.scenario}}) and return number of run 
-#' simulations.
+#' \code{\link{sim.repeat.1.scenario}}).
 #'
 #' @param wd working directory were ALL the simulation files lie
 #' 
@@ -11,15 +10,24 @@
 #' @param batchnr nr of files (batches) containing simulation output based on the 
 #' same pc
 #' 
-#' @return Returns a table with counts on run simulation scenarios
+#' @param bayes logical argument to specify whether to load the simulation results 
+#' obtained with Bayesian (, if \code{bayes = T}; default) or frequentist estimation 
+#' approach
+#' 
+#' @return A dataframe \code{res.batch}.
 #' 
 #' @export
 #'
 #'
 
-sim.load.scenario = function(wd, pc, batchnr = 1){
-
-  filename = paste(c(pc, "bADR_sim", batchnr, ".RData") ,collapse="_")
+sim.load.scenario = function(wd, pc, batchnr = 1, bayes = T){
+  
+  if(bayes == T){
+    filename = paste(c(pc, "bADR_sim", batchnr, ".RData") ,collapse="_")
+  }
+  if(bayes == F){
+    filename = paste(c(pc, "fADR_sim", batchnr, ".RData") ,collapse="_")
+  }
   
   path = paste0(wd, "/", filename)
   
