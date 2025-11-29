349a632658a2f353b3e049c096f0cf2a761a73f9
julia-dyck <julia.alexandra.dyck@gmail.com>
2025-11-29 12:36:23 +0100

    eval.rank: merge b and f fct; edit doc; arg check still missing

--- R/eval.rank_auc_b.R
+++ R/eval.rank_auc_b.R
@@ -1,42 +1,88 @@
-#' Evaluation: rank BWSP tests by AUC
+#' Rank WSP test configurations by AUC obtained from simulation
 #'
-#' Ranks all tried fit & test specifications by AUC.
+#' Ranks all fit (defined by time-to-event and prior distribution) 
+#' and test specifications grouped by simulation scenarios in terms
+#' of the corresponding area under the curve (AUC) value for either Bayesian or 
+#' frequentist Weibull Shape Parameter (BWSP, FWSP) tests.
 #'
-#' @param perf_b A data frame containing Bayesian performance metric results returned by \link{eval.calc_perf_b}.
+#' @param perf_b data frame containing performance results for BWSP tests 
+#'               returned by \link{eval.calc_perf_b}
+#' @param perf_f data frame containing performance results for FWSP tests 
+#'               returned by \link{eval.calc_perf_f} 
 #'
 #' @return A list containing 
 #' \itemize{
-#' \item \code{$ranking}: Ranking of fit and BWSP test specifications according 
-#' to AUC averaged over all sample scenarios given a correct specification of prior belief.
+#' \item \code{$ranking}: Ranking of fit and WSP test specifications according 
+#' to AUC averaged over all sample scenarios (- for BWSP given a correct 
+#' specification of prior belief)
 #' \item \code{$effect.of.N}: Effect of sample size on AUC for the optimal fit 
-#' and BWSP test specification given a correct specification of prior belief.
+#' and WSP test (- for BWSP given a correct specification of prior belief)
 #' \item \code{$effect.of.br}: Effect of background rate on AUC for the optimal fit
-#' and BWSP test specification given a correct specification of prior belief.
+#' and WSP test (- for BWSP given a correct specification of prior belief)
 #' \item \code{$effect.of.adr.rate}: Effect of ADR rate on AUC for the optimal fit
-#' and BWSP test specification given a correct specification of prior belief.
+#' and WSP test (- for BWSP given a correct specification of prior belief)
 #' \item \code{$effect.of.adr.when}: Effect of true expected event times on AUC 
-#' for the optimal fit and BWSP test specification given a correct specification of prior belief.
+#' for the optimal fit and WSP test (- for BWSP given a correct 
+#' specification of prior belief)
 #' \item \code{$effect.of.adr.relsd}: Effect of relative standard deviation of event 
-#' time on AUC for the optimal fit and BWSP test specification given a correct 
-#' specification of prior belief.
+#' time on AUC for the optimal fit and WSP test (- for BWSP given a correct 
+#' specification of prior belief)
 #' \item \code{$effect.of.dist.prior.to.truth}: Effect of distance of prior belief 
-#' to true adr.when on AUC for the optimal fit and BWSP test specification.
+#' to true adr.when on AUC for the optimal fit and WSP test
 #' }
+#' 
+#' @details For definitions of the performance metrics returned in output, 
+#' see the details section of \link{eval.calc_perf}.
 #' 
 #' 
 #' @examples
 #' \dontrun{
-#' # Filtering for a specific tte and prior distribution (for instance based on
-#' # evaluation of execution times, convergence and 
-#'  auc_pgw_ll <- dplyr::filter(perf_b, tte.dist == "pgw", prior.dist == "ll")
-#'  eval.rank_auc_b(auc_dw_ll)
+#' # ranking for all Bayesian WSP tests 
+#' # input perf_b loaded or generated with eval.calc_perf_b
+#' ranking_b = eval.rank_auc_b(perf_b)
+#' # effects of DGP parameters on AUC of top test
+#' ranking_b
+#' 
+#' # ranking for all frequentist WSP tests
+#' # perf_f loaded or generated with eval.calc_perf_f
+#' ranking_f = eval.rank_auc_f(perf_f)
+#' # effects of DGP parameters on AUC of top test
+#' ranking_f
+#' 
+#' # merging AUC ranking of Bayesian and frequentist tests
+#' ranking = dplyr::bind_rows(ranking_b$ranking[, 1:8], 
+#'                            ranking_f$ranking[, 1:5]) %>%
+#'           filter(tte.dist == "pgw") %>%
+#'           dplyr::arrange(dplyr::desc(AUC))
+#'       
+#'     
+#' # ranking among subset of scenarios, e.g. excluding model choices that lead to
+#' # long execution times
+#' auc_pgw_ll <- dplyr::filter(perf_b, tte.dist == "pgw", prior.dist == "ll")
+#' eval.rank_auc_b(auc_dw_ll) 
+#'                                
 #' }
-#' @export
+#' 
+#'
+#' @name eval.rank_auc
+#'
+NULL
 
+
+#' @rdname eval.rank_auc
+#' 
+#' @export
+#'
+
+
+# for the Bayesian tests 
 
 eval.rank_auc_b = function(perf_b){
   require(dplyr)
  
+  # 0. argument checks
+  # TODO
+  
   tab.df = perf_b
   
   # 1. summarize and rank fit & test specifications
@@ -127,3 +173,86 @@ eval.rank_auc_b = function(perf_b){
   
 }
 
+
+
+
+#' @rdname eval.rank_auc
+#' 
+#' @export
+#'
+
+
+## fct for frequentist tests
+
+eval.rank_auc_f = function(perf_f){
+  require(dplyr)
+  
+  tab.df = perf_f
+  
+  # 1. summarize and rank fit & test specifications
+  
+  # ranking
+  tab.ranked <- tab.df %>%
+    dplyr::group_by(tte.dist, cred.level) %>% # group by fit&test specifications
+    dplyr::summarise(AUC = mean(auc), FPR = mean(fpr), TPR = mean(tpr), FNR = mean(fnr), TNR = mean(tnr),
+                     .groups = "drop") %>%
+    dplyr::arrange(dplyr::desc(AUC)) # ranking
+  
+  
+  # 2. filter for best fit & test specification and investigate effect of scenario parameters 
+  
+  opti.tte.dist = tab.ranked$tte.dist[1]
+  opti.cred.level = tab.ranked$cred.level[1]
+  
+  # optimal fit & test specification according to ranking under correct prior belief
+  tab.opti = tab.df %>% filter(tte.dist == opti.tte.dist, 
+                               cred.level == opti.cred.level,
+  )
+  
+  # Effect of N
+  tab.opti.N = tab.opti %>% 
+    group_by(N) %>% 
+    dplyr::summarise(AUC = mean(auc), FPR = mean(fpr), TPR = mean(tpr), FNR = mean(fnr), TNR = mean(tnr),
+                     .groups = "drop")
+  
+  # Effect of br
+  tab.opti.br = tab.opti %>% 
+    group_by(br) %>% 
+    dplyr::summarise(AUC = mean(auc), FPR = mean(fpr), TPR = mean(tpr), FNR = mean(fnr), TNR = mean(tnr), 
+                     .groups = "drop")
+  
+  # Effect of adr.rate
+  tab.opti.adr.rate = tab.opti %>% 
+    group_by(adr.rate) %>% 
+    dplyr::summarise(AUC = mean(auc), FPR = mean(fpr), TPR = mean(tpr), FNR = mean(fnr), TNR = mean(tnr),
+                     .groups = "drop")
+  
+  # Effect of adr.when
+  tab.opti.adr.when = tab.opti %>% 
+    group_by(adr.when) %>% 
+    dplyr::summarise(AUC = mean(auc), FPR = mean(fpr), TPR = mean(tpr), FNR = mean(fnr), TNR = mean(tnr),
+                     .groups = "drop")
+  
+  # Effect of adr.relsd
+  tab.opti.adr.relsd = tab.opti %>% 
+    group_by(adr.relsd) %>% 
+    dplyr::summarise(AUC = mean(auc), FPR = mean(fpr), TPR = mean(tpr), FNR = mean(fnr), TNR = mean(tnr),
+                     .groups = "drop")
+  
+  # Output 
+  out = list(
+    ranking = tab.ranked,
+    effect.of.N = tab.opti.N,
+    effect.of.br = tab.opti.br,
+    effect.of.adr.rate = tab.opti.adr.rate,
+    effect.of.adr.when = tab.opti.adr.when,
+    effect.of.adr.relsd = tab.opti.adr.relsd
+  )
+  return(out)
+  
+}
+
+
+
+
+## END OF DOC
