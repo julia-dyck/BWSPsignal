b23affd91f5a13d3f27184013c86a878dffb7a39
julia-dyck <julia.alexandra.dyck@gmail.com>
2026-01-08 09:42:33 +0100

    minimal editing documentation

--- R/tte.R
+++ R/tte.R
@@ -1,11 +1,11 @@
-#' test data for development of simstudy code
+#' test time-to-event dataset
 #'
-#' Simulated data generated with \code{sim.datagen_tte(c(100, 0.1, 1, 0.5, 0.05, 365))}.
+#' Simulated time-to-event (tte) data generated with \code{sim.datagen_tte(c(100, 0.1, 1, 0.5, 0.05, 365))}.
 #' 
 #' @format  A data frame with 100 rows and 2 variables:
 #' \describe{
 #'   \item{time}{event-time if an event was observed, or censoring time if no event was observed,}
-#'   \item{status}{event status; 1 if an event was observed, 0 if no event was observed,}
+#'   \item{status}{event status; 1 if an event was observed, 0 if no event was observed.}
 #'}
 #' 
 #' @seealso \code{\link{sim.datagen_tte}}
