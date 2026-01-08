cee4f026a4f176c7d3c48f0ddcb8cf4c79c2d5fd
julia-dyck <julia.alexandra.dyck@gmail.com>
2026-01-08 12:25:26 +0100

    add simulated dataset to package

---
+++ R/muscu.R
@@ -0,0 +1,23 @@
+#' Simulated musculoskeletal pain time-to-event dataset
+#'
+#' A simulated time-to-event (tte) dataset representing musculoskeletal pain
+#' following bisphosphonate intake.
+#'
+#' @format
+#' A data frame with 19 777 rows and 2 variables:
+#' \describe{
+#'   \item{time}{event time (in days) or censoring time (365 days),}
+#'   \item{status}{event indicator; 1 = event observed, 0 = censored.}
+#' }
+#'
+#' @details
+#' The data was generated using 
+#' \code{\link{sim.datagen_tte}} 
+#' with parameters
+#' derived from the case study presented in \insertCite{dyck2024bpgwsppreprint;textual}{WSPsignal}, 
+#' namely N = ...
+#'
+#' @seealso \code{\link{sim.datagen_tte}}
+#' @references \insertAllCited{}
+#'
+"muscu"
