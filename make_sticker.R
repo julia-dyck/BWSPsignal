a0644a98a1399113643f310b9c5f80a39ea4e6cd
julia-dyck <julia.alexandra.dyck@gmail.com>
2026-01-25 20:24:56 +0100

    init hexagon sticker

---
+++ make_sticker.R
@@ -0,0 +1,20 @@
+### package sticker
+
+library(hexSticker)
+
+sticker(
+  subplot = "flag.png",   # or .svg
+  p_color = "black",
+  s_x = 0.9,
+  s_y = 0.8,
+  s_width = 1,
+  package = "WSPsignal",
+  p_size = 16,
+  p_y = 1.5,
+  h_color = "black",
+  h_fill = "#00FFD0",
+  filename = "Logo_WSPsignal.png",
+  dpi = 300
+)
+
+
